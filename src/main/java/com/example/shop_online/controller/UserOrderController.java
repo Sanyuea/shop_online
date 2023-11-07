3625834d812efa604687346dc8ea70b64ac0f8ac
Sanyuea <1170557347@qq.com>
2023-11-07 13:49:44 +0800

    搭建项目基础框架&配置项目相关实体

---
+++ src/main/java/com/example/shop_online/controller/UserOrderController.java
@@ -0,0 +1,18 @@
+package com.example.shop_online.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author yule
+ * @since 2023-11-07
+ */
+@RestController
+@RequestMapping("/shop_online/userOrder")
+public class UserOrderController {
+
+}
