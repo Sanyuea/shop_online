fa64a34320e635097d6b6cf2b7545833a4055b2e
Sanyuea <1170557347@qq.com>
2023-11-11 14:38:20 +0800

    首页轮播图

--- src/main/java/com/example/shop_online/service/impl/IndexCarouselServiceImpl.java
+++ src/main/java/com/example/shop_online/service/impl/IndexCarouselServiceImpl.java
@@ -1,10 +1,13 @@
 package com.example.shop_online.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.example.shop_online.entity.IndexCarousel;
 import com.example.shop_online.mapper.IndexCarouselMapper;
 import com.example.shop_online.service.IndexCarouselService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +19,17 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class IndexCarouselServiceImpl extends ServiceImpl<IndexCarouselMapper, IndexCarousel> implements IndexCarouselService {
+    @Override
+    public List<IndexCarousel> getList(Integer distributionSite){
+        LambdaQueryWrapper<IndexCarousel> wrapper = new LambdaQueryWrapper<>();
+        if (distributionSite != null){
+            wrapper.eq(IndexCarousel::getType,distributionSite);
+        }
+        //设置排序根据创建时间倒叙
+        wrapper.orderByDesc(IndexCarousel::getCreateTime);
+        //查询广告列表
+        List<IndexCarousel> list = baseMapper.selectList(wrapper);
+        return list;
+    }
 
 }
