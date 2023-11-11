9e59f63ff82e4da0811cdad87232b91809860e92
Sanyuea <1170557347@qq.com>
2023-11-11 14:53:49 +0800

    推荐管理&分类管理

--- src/main/java/com/example/shop_online/service/CategoryService.java
+++ src/main/java/com/example/shop_online/service/CategoryService.java
@@ -3,6 +3,8 @@ package com.example.shop_online.service;
 import com.example.shop_online.entity.Category;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务类
@@ -12,5 +14,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-11-07
  */
 public interface CategoryService extends IService<Category> {
-
+    List<Category> getIndexCategoryList();
 }
--- src/main/java/com/example/shop_online/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/shop_online/service/impl/CategoryServiceImpl.java
@@ -1,10 +1,16 @@
 package com.example.shop_online.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.example.shop_online.entity.Category;
+import com.example.shop_online.enums.CategoryRecommendEnum;
 import com.example.shop_online.mapper.CategoryMapper;
+import com.example.shop_online.mapper.GoodsMapper;
 import com.example.shop_online.service.CategoryService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -15,6 +21,18 @@ import org.springframework.stereotype.Service;
  * @since 2023-11-07
  */
 @Service
+@AllArgsConstructor
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
+    private final GoodsMapper goodsMapper;
+    @Override
+    public List<Category> getIndexCategoryList(){
+        LambdaQueryWrapper<Category> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Category::getIsRecommend, CategoryRecommendEnum.ALL_RECOMMEND.getValue());
+        wrapper.orderByDesc(Category::getCreateTime);
+        List<Category> list = baseMapper.selectList(wrapper);
+        return list;
+
+
+    }
 
 }
