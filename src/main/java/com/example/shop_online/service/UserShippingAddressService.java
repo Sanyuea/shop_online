af514860013d960db769eb8205cfa82ae5ca9b1d
Sanyuea <1170557347@qq.com>
2023-11-12 22:27:58 +0800

    地址模块功能

--- src/main/java/com/example/shop_online/service/UserShippingAddressService.java
+++ src/main/java/com/example/shop_online/service/UserShippingAddressService.java
@@ -2,6 +2,9 @@ package com.example.shop_online.service;
 
 import com.example.shop_online.entity.UserShippingAddress;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.shop_online.vo.AddressVO;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +15,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-11-07
  */
 public interface UserShippingAddressService extends IService<UserShippingAddress> {
-
+    Integer saveShippingAddress(AddressVO addressVO);
+    Integer editShippingAddress(AddressVO addressVO);
+    List<AddressVO> getList(Integer userId);
+    AddressVO getAddress(Integer id);
+    Integer deleteShippingAddress(Integer id);
 }
