b3131a706214f4e460495a886491ba3748346d29
Sanyuea <1170557347@qq.com>
2023-11-18 12:34:59 +0800

    tab分类接口更新

--- src/main/java/com/example/shop_online/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/shop_online/service/impl/CategoryServiceImpl.java
@@ -29,33 +29,38 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
+
     private final GoodsMapper goodsMapper;
-    @Override
-    public List<Category> getIndexCategoryList(){
+
+    public List<Category> getIndexCategoryList() {
         LambdaQueryWrapper<Category> wrapper = new LambdaQueryWrapper<>();
-        wrapper.eq(Category::getIsRecommend, CategoryRecommendEnum.ALL_RECOMMEND.getValue());
+        // 查询首页和分类页都推荐的分类以及在首页推荐的分类
+        wrapper.eq(Category::getIsRecommend, CategoryRecommendEnum.ALL_RECOMMEND.getValue()).or().eq(Category::getIsRecommend, CategoryRecommendEnum.INDEX_RECOMMEND.getValue());
         wrapper.orderByDesc(Category::getCreateTime);
-        List<Category> list = baseMapper.selectList(wrapper);
-        return list;
+        wrapper.orderByDesc(Category::getCreateTime);
+        return baseMapper.selectList(wrapper);
     }
 
     @Override
-    public List<CategoryVO> getCategoryList(){
+    public List<CategoryVO> getCategoryList() {
         List<CategoryVO> list = new ArrayList<>();
+        // 1、查询配置在分类tab页上的父级分类
         LambdaQueryWrapper<Category> wrapper = new LambdaQueryWrapper<>();
-        wrapper.eq(Category::getIsRecommend,CategoryRecommendEnum.ALL_RECOMMEND.getValue()).or();
+        wrapper.eq(Category::getIsRecommend, CategoryRecommendEnum.ALL_RECOMMEND.getValue()).or().eq(Category::getIsRecommend, CategoryRecommendEnum.CATEGORY_HOME_RECOMMEND.getValue());
         List<Category> categories = baseMapper.selectList(wrapper);
+        // 2、查询该分类下的自己分类
         LambdaQueryWrapper<Goods> queryWrapper = new LambdaQueryWrapper<>();
-        for (Category category : categories){
+        for (Category category : categories) {
             CategoryVO categoryVO = new CategoryVO();
-            categoryVO.setId(categoryVO.getId());
-            categoryVO.setName(categoryVO.getName());
-            categoryVO.setIcon(categoryVO.getIcon());
+            categoryVO.setId(category.getId());
+            categoryVO.setName(category.getName());
+            categoryVO.setIcon(category.getIcon());
             wrapper.clear();
-            wrapper.eq(Category::getParentId,category.getId());
+            wrapper.eq(Category::getParentId, category.getId());
             List<Category> childCategories = baseMapper.selectList(wrapper);
             List<CategoryChildrenGoodsVO> categoryChildrenGoodsList = new ArrayList<>();
-            for (Category item : childCategories){
+            // 3、分类下的商品列表
+            for (Category item : childCategories) {
                 CategoryChildrenGoodsVO childrenGoodsVO = new CategoryChildrenGoodsVO();
                 childrenGoodsVO.setId(item.getId());
                 childrenGoodsVO.setName(item.getName());
@@ -63,7 +68,7 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> i
                 childrenGoodsVO.setParentId(category.getId());
                 childrenGoodsVO.setParentName(category.getName());
                 queryWrapper.clear();
-                List<Goods> goodsList = goodsMapper.selectList(queryWrapper.eq(Goods::getCategoryId,item.getId()));
+                List<Goods> goodsList = goodsMapper.selectList(queryWrapper.eq(Goods::getCategoryId, item.getId()));
                 List<RecommendGoodsVO> goodsVOList = GoodsConvert.INSTANCE.convertToRecommendGoodsVOList(goodsList);
                 childrenGoodsVO.setGoods(goodsVOList);
                 categoryChildrenGoodsList.add(childrenGoodsVO);
@@ -71,8 +76,6 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> i
             categoryVO.setChildren(categoryChildrenGoodsList);
             list.add(categoryVO);
         }
-
         return list;
     }
-
 }
