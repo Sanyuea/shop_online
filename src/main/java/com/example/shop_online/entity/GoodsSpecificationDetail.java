240c21ec579e4c06e67f4b453d8488fa11637bb6
Sanyuea <1170557347@qq.com>
2023-11-21 08:07:55 +0800

    商品详情接口更新

--- src/main/java/com/example/shop_online/entity/GoodsSpecificationDetail.java
+++ src/main/java/com/example/shop_online/entity/GoodsSpecificationDetail.java
@@ -1,5 +1,7 @@
 package com.example.shop_online.entity;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
@@ -7,6 +9,10 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.time.LocalDateTime;
+import java.util.List;
+
+import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
+import com.google.gson.JsonObject;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -22,7 +28,7 @@ import lombok.Setter;
  */
 @Getter
 @Setter
-@TableName("t_goods_specification_detail")
+@TableName(value = "t_goods_specification_detail" ,autoResultMap = true)
 @ApiModel(value = "GoodsSpecificationDetail对象", description = "")
 public class GoodsSpecificationDetail {
 
@@ -51,8 +57,8 @@ public class GoodsSpecificationDetail {
     private Double oldPrice;
 
     @ApiModelProperty("规格详情")
-    @TableField("specs")
-    private String specs;
+    @TableField(value = "specs",typeHandler = JacksonTypeHandler.class)
+    private List<JSONObject> specs;
 
     @ApiModelProperty("逻辑删除(0-未删除，1-已删除)")
     @TableField("delete_flag")
